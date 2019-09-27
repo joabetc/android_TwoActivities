d47454fbf80d3b495aee5fa769500844470cfc2c
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-09-27 16:22:17 -0300

    Creating a response Intent in the second Activity

--- app/src/main/java/com/joabe/twoactivities/SecondActivity.java
+++ app/src/main/java/com/joabe/twoactivities/SecondActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private EditText mReply;
+
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +23,14 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+        mReply = findViewById(R.id.editText_second);
     }
 
     public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
